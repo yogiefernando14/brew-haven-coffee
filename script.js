886f1bc6c993399c9435ba7ace66a7450f67e008
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-03 19:46:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const reveals = document.querySelectorAll(".reveal");
 
+function revealOnScroll(){
+
+reveals.forEach(item=>{
+
+const top = item.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+item.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+revealOnScroll();
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY > 50){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
+
+const menuToggle = document.getElementById("menuToggle");
+
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click",()=>{
+
+navMenu.classList.toggle("active");
+
+});
